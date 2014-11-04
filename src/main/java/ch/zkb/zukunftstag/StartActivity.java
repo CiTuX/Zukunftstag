14dd1ec5cf8c15ff8a5eed8073ddcafdb1a729b1
CiTuX <info@citux.ch>
2014-11-04 19:29:32 +0100

    styling first layout

--- src/main/java/ch/zkb/zukunftstag/StartActivity.java
+++ src/main/java/ch/zkb/zukunftstag/StartActivity.java
@@ -2,13 +2,27 @@ package ch.zkb.zukunftstag;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class StartActivity extends ActionBarActivity {
+
+    TextView txtSample;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        txtSample = (TextView) findViewById(R.id.txtSample);
+    }
+
+    public void buttonClick(View view) {
+        String text = txtSample.getText().toString();
+        if (!text.matches("")) {
+            Toast.makeText(this, txtSample.getText(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
